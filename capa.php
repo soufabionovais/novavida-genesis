499e6226127a87a04f0a5271f97949bccbccd261
lilian_promenzio <lilian.promenzio@gmail.com>
2018-10-14 17:40:40 -0300

    Aplicado conteúdo da tela de Capa.

--- capa.php
+++ capa.php
@@ -41,7 +41,40 @@
 					</div>
 
 					<div class="doc-pagina capa">
-						
+						<div class="conteudo-wrapper">
+							<div class="item">
+								<span class="item-nome">Critério Consultado:</span>
+								<span class="item-valor">111.111.111-11</span>
+							</div>
+							
+							<div class="item">
+								<span class="item-nome">Nome:</span>
+								<span class="item-valor">Joaquim Antônio da Silva</span>
+							</div>
+							
+							<div class="item">
+								<span class="item-nome">Homônimo:</span>
+								<span class="item-valor">Joaquim Antônio da Silva</span>
+							</div>
+
+							<div class="item duas-colunas">
+								<div class="item-col">
+									<span class="item-nome">Criado em:</span>
+									<span class="item-valor">15 de janeiro de 2018</span>
+								</div>
+								<div class="item-col">
+									<span class="item-nome">Última atualização</span>
+									<span class="item-valor">18 de junho de 2018</span>
+								</div>
+							</div>
+							
+							<div class="item centralizado">
+								<span class="item-nome">Pessoas envolvidas na análise:</span>
+								<span class="item-valor">
+								Wanderley Marmello - Dpto. Jurídico | Shirley Sanches - Dpto. de cobrança Cássio Ferreira Martins - Análise de Crédito
+								</span>
+							</div>
+						</div>
 					</div>
 
 				</div>
