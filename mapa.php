8c7d935b77e439f69f501fd71dd21597ff250576
lilian_promenzio <lilian.promenzio@gmail.com>
2018-10-20 00:27:09 -0300

    Criada base do mapa.

---
+++ mapa.php
@@ -0,0 +1,28 @@
+<?php include("includes/head.php"); ?>
+  <body>
+    
+    <?php include("includes/header-monitoria.php"); ?>
+
+		<main class="ls-main mapa">
+			<div id="mapaWrapper" class="mapa-wrapper">
+				
+				<div class="item-mapa item-usuario">
+					<img src="assets/img/avatar-usuario.png" alt="" class="avatar" />
+
+					<button class="btn btn-agrupamentos">
+						<span class="icon nv-icon-zoom"></span>
+					</button>
+
+					<div class="dados">
+						<div class="nome">André Cintra</div>
+						<div class="documento">CPF: 999.999.999-99</div>
+					</div>
+				</div>
+
+			</div>
+		</main>
+
+		<?php include("includes/scripts.php"); ?>
+		<script src="assets/js/doc.js"></script>
+	</body>
+</html>
