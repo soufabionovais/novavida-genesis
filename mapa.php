b2f9e2f8258c9c75207430d4b6d29e437943a5f0
lilian_promenzio <lilian.promenzio@gmail.com>
2018-10-20 02:50:19 -0300

    criado menu de agrupamento de fonte.

--- mapa.php
+++ mapa.php
@@ -1,11 +1,11 @@
 <?php include("includes/head.php"); ?>
   <body>
-    
+
     <?php include("includes/header-monitoria.php"); ?>
 
 		<main class="ls-main mapa">
 			<div id="mapaWrapper" class="mapa-wrapper">
-				
+
 				<div class="item-mapa item-usuario">
 					<img src="assets/img/avatar-usuario.png" alt="" class="avatar" />
 
@@ -17,6 +17,52 @@
 						<div class="nome">André Cintra</div>
 						<div class="documento">CPF: 999.999.999-99</div>
 					</div>
+
+					<section class="ls-box menu-agrupamentos">
+						<div id="etapa1" class="etapa-1">
+							<header>
+								  Agrupamentos
+							</header>
+							<button class="item-line">
+								<span class="texto-destaque">Agrupamento Fonte 1</span>
+								<div class="nv-icon-calendar">5 dias</div>
+							</button>
+							<button class="item-line">
+								<span class="texto-destaque">Agrupamento Fonte 1</span>
+								<div class="nv-icon-calendar">5 dias</div>
+							</button>
+							<button class="item-line">
+								<span class="texto-destaque">Agrupamento Fonte 1</span>
+								<div class="nv-icon-calendar">5 dias</div>
+							</button>
+							<button class="item-line">
+								<span class="texto-destaque">Agrupamento Fonte 1</span>
+								<div class="nv-icon-calendar">5 dias</div>
+							</button>
+						</div>
+
+						<div id="etapa2" class="etapa-2">
+							<header>
+								<span class="nv-icon-arrow-left-thin">Agrupamento fonte 1</span>
+							</header>
+							<button class="item-line">
+								<span class="texto-destaque">Fonte 1</span>
+								<div class="nv-icon-calendar">5 dias</div>
+							</button>
+							<button class="item-line">
+								<span class="texto-destaque">Fonte 1</span>
+								<div class="nv-icon-calendar">5 dias</div>
+							</button>
+							<button class="item-line">
+								<span class="texto-destaque">Fonte 1</span>
+								<div class="nv-icon-calendar">5 dias</div>
+							</button>
+							<button class="item-line">
+								<span class="texto-destaque">Fonte 1</span>
+								<div class="nv-icon-calendar">5 dias</div>
+							</button>
+						</div>
+                    </section>
 				</div>
 
 			</div>
